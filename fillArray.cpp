171f86113b97c982a9df27dba1d680327cb8d575
Movses-Miqayelyan <movses.miqayelyan@student.rau.am>
2025-09-19 11:44:18 +0400

    Add files via upload

---
+++ fillArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+void fillArray(std::array<int, 7>& arr, int value){
+    for (auto& elem : arr) {
+        elem = value;
+    }
+}
+
+int main(){
+    
+    std::array<int, 7> myArr; 
+
+    fillArray(myArr, 7);
+    
+    for (int x : myArr) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
